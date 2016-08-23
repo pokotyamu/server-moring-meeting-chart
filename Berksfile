f6ec2b1a6ef224a6a4d20c6d3cb377396dcbd3a6
pokotyamu <pokotyamu@gmail.com>
2016-08-23 11:58:14 +0900

    nginx の導入

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://api.berkshelf.com"
 
 cookbook 'git'
+cookbook 'yum-epel'
+cookbook 'nginx', path: "./site-cookbooks/nginx"
--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "yum-epel"
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [ :enable, :start ]
+  supports :status => true, :restart => true, :reload => true
+end
